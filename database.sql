cdc30010099004d9c0a0aff5a9316ef40040be27
Alche Paye <payealche816@gmail.com>
2026-05-09 17:59:32 +0800

    Create student_management database and students table

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    course VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
